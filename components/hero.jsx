45cb12b2a4a554c57c2aa29054c536fd4870657b
Piyush Agarwal <piyushagarwalvo@gmail.com>
2025-01-21 13:24:56 +0530

    fix hero section

--- components/hero.jsx
+++ components/hero.jsx
@@ -27,7 +27,7 @@ const HeroSection = () => {
   }, []);
 
   return (
-    <section className="w-full py-36 pt-48 ">
+    <section className="w-full pt-36 md:pt-48 pb-4">
       <div className=" space-y-6 text-center">
         <div className="space-y-6 mx-auto">
           <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title animate-gradient">
